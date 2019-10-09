c230629b5692f6c6d0c40281ab9ef2dfd32c32ab
Mike <2114794@talnet.nl>
2019-10-09 14:38:08 +0200

    Uitwerking van de exercise

---
+++ hello.php
@@ -0,0 +1,19 @@
+<?php
+
+$help = "Dit was veels te moeilijk voor geen reden";
+$txt = "Dit is text";
+$ok = "ok";
+$xd = "yuuge";
+
+echo "Hello World!";
+echo PHP_EOL;
+echo "$help";
+echo PHP_EOL;
+echo "$txt";
+echo PHP_EOL;
+echo "$ok";
+echo PHP_EOL;
+echo "$xd";
+echo PHP_EOL;
+
+?>
